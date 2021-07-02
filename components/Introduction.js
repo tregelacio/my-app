3ede9fac84747659256f9dc3c7d8343ebdd12c43
tregelacio <tre.gelacio@gmail.com>
2021-07-01 19:37:50 -1000

    Created components and responsive site actions

---
+++ components/Introduction.js
@@ -0,0 +1,20 @@
+import {
+    Link,
+    Text,
+    Stack,
+    Heading,
+    Box,
+    Button,
+    SlideFade,
+    Image
+} from '@chakra-ui/react'
+import { SiGithub, SiLinkedin, SiGmail } from 'react-icons/si'
+import useMediaQuery from '../hook/useMediaQuery'
+import ReactGA from 'react-ga'
+
+export default function Introduction() {
+    return (
+        <>
+        </>
+    )
+}
